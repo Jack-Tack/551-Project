5d12d9b3a973de4f7eb0e4c32d95cb2401f15435
Jack-Tack <98119798+Jack-Tack@users.noreply.github.com>
2024-04-27 20:52:35 -0400

    Add get functions & fix load functions

--- Recommender.py
+++ Recommender.py
@@ -6,7 +6,6 @@ from Show import Show
 import tkinter.filedialog
 import os
 
-
 class Recommender:
     def __init__(self):
         # self.__book = {Book.getID(book): book}
@@ -20,11 +19,10 @@ class Recommender:
         filename = tkinter.filedialog.askopenfilename(title="Files", initialdir=os.getcwd())
         while not os.path.exists(f"{filename}"):
             filename = tkinter.filedialog.askopenfilename(title="Files", initialdir=os.getcwd())
-
         with open(filename, 'r') as file:
             for line in file:
                 book_info = line.strip().split(",")
-                id, title, rating, authors, isbn, isbn13, lang, numPage, numRate, pubDate, pub = book_info
+                id, title, authors, rating, isbn, isbn13, lang, numPage, numRate, pubDate, pub = book_info
                 book = Book(id, title, rating, authors, isbn, isbn13, lang, numPage, numRate, pubDate, pub)
                 self.__books[id] = book
 
@@ -32,12 +30,11 @@ class Recommender:
         filename = tkinter.filedialog.askopenfilename(title="Files", initialdir=os.getcwd())
         while not os.path.exists(f"{filename}"):
             filename = tkinter.filedialog.askopenfilename(title="Files", initialdir=os.getcwd())
-
         with open(filename, 'r') as file:
             for line in file:
                 show_info = line.strip().split(",")
-                id, title, rating, directors, actors, countryCode, dateAdded, releaseYear, showRate, duration, genre, description = show_info
-                show = Show(id, title, rating, directors, actors, countryCode, dateAdded, releaseYear, showRate, duration, genre, description)
+                id, type, title, directors, actors, rating, countryCode, dateAdded, releaseYear, showRate, duration, genre, description = show_info
+                show = Show(id, type, title, rating, directors, actors, countryCode, dateAdded, releaseYear, showRate, duration, genre, description)
                 self.__shows[id] = show
 
     def loadAssociations(self):
@@ -60,4 +57,25 @@ class Recommender:
                 else:
                     self.__associations[second_id][first_id] += 1
 
+    def getMovieList(self):
+        title_width = max(len(show.title) for show in self.__shows.values() if show.type == "Movie")
+        run_width = max(len(show.duration) for show in self.__shows.values() if show.type == "Movie")
+        print(f"{'Title':<{title_width}} {'Runtime':<{run_width}}")
+        for show in self.__shows.values():
+            if show.type == "Movie":
+                print(f"{show.title:<{title_width}} {show.duration:<{run_width}}")
 
+    def getTVList(self):
+        title_width = max(len(show.title) for show in self.__shows.values() if show.type != "Movie")
+        s_width = len("Seasons")
+        print(f"{'Title':<{title_width}} {'Seasons':<{s_width}}")
+        for show in self.__shows.values():
+            if show.type != "Movie":
+                print(f"{show.title:<{title_width}} {show.seasons:<{s_width}}")
+
+    def getBookList(self):
+        title_width = max(len(book.title) for book in self.__books.values())
+        auth_width = max(len(book.authors) for book in self.__books.values())
+        print(f"{'Title':<{title_width}} {'Authors':<{auth_width}}")
+        for book in self.__books.values():
+            print(f"{book.title:<{title_width}} {book.authors:<{auth_width}}")
