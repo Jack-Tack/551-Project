6bb5c2fe306c8c28bd070316f22c3eb37b59a8f3
Jarrett Aaronson <156959670+JarrettAaronson@users.noreply.github.com>
2024-05-04 14:36:52 -0400

    Update RecommenderGUI.py

--- RecommenderGUI.py
+++ RecommenderGUI.py
@@ -15,18 +15,26 @@ class RecommenderGUI:
         self.__nb.pack(expand=1, fill=tkinter.BOTH)
         self.__movieTab = ttk.Frame(self.__nb)
         self.__showsTab = ttk.Frame(self.__nb)
+        self.__booksTab = ttk.Frame(self.__nb)
         self.__nb.add(self.__movieTab, text="Movies")
         self.__nb.add(self.__showsTab, text="TV Shows")
+        self.__nb.add(self.__booksTab, text="Books")
         self.__movieText = tkinter.Text(self.__movieTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
         self.__movieStats = tkinter.Text(self.__movieTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
         self.__showsText = tkinter.Text(self.__showsTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
         self.__showsStats = tkinter.Text(self.__showsTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
+        self.__booksText = tkinter.Text(self.__booksTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
+        self.__booksStats = tkinter.Text(self.__booksTab, wrap=tkinter.WORD, state=tkinter.DISABLED)
         self.__movieText.pack(expand=1, fill=tkinter.BOTH)
         self.__movieStats.pack(expand=1, fill=tkinter.BOTH)
         self.__showsText.pack(expand=1, fill=tkinter.BOTH)
         self.__showsStats.pack(expand=1, fill=tkinter.BOTH)
+        self.__booksText.pack(expand=1, fill=tkinter.BOTH)
+        self.__booksStats.pack(expand=1, fill=tkinter.BOTH)
         self.__loadShowsButton = tkinter.Button(self.__main_window, text="Load Shows", command=self.loadShows)
         self.__loadShowsButton.pack(expand=1)
+        self.__loadBooksButton = tkinter.Button(self.__main_window, text="Load Books", command=self.loadBooks)
+        self.__loadBooksButton.pack(expand=1)
 
     def loadShows(self):
         self.__movieText.configure(state=tkinter.NORMAL)
@@ -71,8 +79,29 @@ class RecommenderGUI:
         self.__showsText.configure(state=tkinter.DISABLED)
         self.__showsStats.configure(state=tkinter.DISABLED)
 
+    def loadBooks(self):
+        self.__booksText.configure(state=tkinter.NORMAL)
+        self.__booksStats.configure(state=tkinter.NORMAL)
+        self.__recommender.loadBooks()
+        self.__booksText.delete("1.0", tkinter.END)
+        self.__booksStats.delete("1.0", tkinter.END)
+        bookList = self.__recommender.getBookList()
+        for book in bookList:
+            self.__booksText.insert(tkinter.END, book + "\n")
+        avgPage, maxAuthor, maxPublisher = self.__recommender.getBookStats()
+        self.__booksStats.insert(tkinter.END, "\n")
+        self.__booksStats.insert(tkinter.END, "Average Page Count: " + str(avgPage) + " pages" + "\n")
+        self.__booksStats.insert(tkinter.END, "\n")
+        self.__booksStats.insert(tkinter.END, "Most Prolific Author: " + str(maxAuthor) + "\n")
+        self.__booksStats.insert(tkinter.END, "\n")
+        self.__booksStats.insert(tkinter.END, "Most Prolific Publisher: " + str(maxPublisher) + "\n")
+        self.__booksText.configure(state=tkinter.DISABLED)
+        self.__booksStats.configure(state=tkinter.DISABLED)
 def main():
     app = RecommenderGUI()
     tkinter.mainloop()
 
 main()
+
+
+
