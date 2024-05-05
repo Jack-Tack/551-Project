d13743c28400795855ee51ce8f2d9ec059108b58
Jack-Tack <98119798+Jack-Tack@users.noreply.github.com>
2024-05-05 17:44:34 -0400

    Update RecommenderGUI.py

--- RecommenderGUI.py
+++ RecommenderGUI.py
@@ -5,7 +5,6 @@ from Recommender import Recommender
 import tkinter
 from tkinter import ttk
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class RecommenderGUI:
